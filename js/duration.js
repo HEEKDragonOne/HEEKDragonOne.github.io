df4c9d1329c579bca2b3a0f25ba0633cc767e608
HEEKDragonOne <1425138134@qq.com>
2022-05-20 17:17:42 +0800

    Site updated: 2022-05-20 17:17:42

--- js/duration.js
+++ js/duration.js
@@ -20,7 +20,7 @@
     if(String(snum).length === 1 ){
       snum = "0" + snum;
     }
-    document.getElementById("timeDate").innerHTML = "本空间站已飞行&nbsp"+dnum+"&nbsp天";
+    document.getElementById("timeDate").innerHTML = "本空间站已运行&nbsp"+dnum+"&nbsp天";
     document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
   }
   setInterval(update, 1000);
